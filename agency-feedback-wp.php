8b0639c5ce32f8d3435f63c10807e9a1fc5ee668
Lyness Lim <lynesslim@gmail.com>
2026-05-04 09:35:22 +0800

    Restructure: plugin at root level

---
+++ agency-feedback-wp.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: Agency Feedback WP
+ * Description: Visual feedback widget powered by Supercraft.
+ * Version: 0.5.0
+ * Author: Supercraft
+ * Requires PHP: 8.0
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('AFWP_VERSION', '0.5.0');
+define('AFWP_FILE', __FILE__);
+define('AFWP_DIR', plugin_dir_path(__FILE__));
+define('AFWP_URL', plugin_dir_url(__FILE__));
+
+require_once AFWP_DIR . 'vendor/plugin-update-checker.php';
+$githubUpdater = YahnisElsts\PluginUpdateChecker\v5p6\PucFactory::buildUpdateChecker(
+    'https://github.com/lynesslim/visual-feedback-plugin',
+    __FILE__,
+    'agency-feedback-wp'
+);
+$githubUpdater->setBranch('main');
+
+require_once AFWP_DIR . 'includes/class-afwp-settings.php';
+require_once AFWP_DIR . 'includes/class-afwp-plugin.php';
+
+function afwp_bootstrap(): void
+{
+    static $instance = null;
+    if ($instance instanceof AFWP_Plugin) {
+        return;
+    }
+    $instance = new AFWP_Plugin();
+    $instance->init();
+}
+
+afwp_bootstrap();
